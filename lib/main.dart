06501bdf103f4e4dc96f983df56cff8e00f3fa5c
kaka0913 <kabuyuu0913@gmail.com>
2024-02-13 21:57:12 +0900

    riverpod導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 // Project imports:
 import 'package:calender_application/view/home_page.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-      const ProviderScope(
+    const ProviderScope(
       child: MyApp(),
-    ),);
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
