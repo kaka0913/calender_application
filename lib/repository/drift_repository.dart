cc6276d84e14ec543d14e15448b61666d5269e4e
kaka0913 <kabuyuu0913@gmail.com>
2024-02-13 20:05:47 +0900

    import drift

---
+++ lib/repository/drift_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:io'; 
+import 'package:drift/native.dart';  
+import 'package:path/path.dart' as p; 
+import 'package:path_provider/path_provider.dart';  
+import 'package:drift/drift.dart';
+
+part 'drift_repository.g.dart';
+
+class Schedules extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  DateTimeColumn get day => dateTime()();
+  DateTimeColumn get startTime => dateTime()();
+  DateTimeColumn get endTime => dateTime()();
+  BoolColumn get isAllDay => boolean()();
+}
+
+@DriftDatabase(tables: [Schedules])
+class SckeduleDatabase extends _$SckeduleDatabase {
+  SckeduleDatabase() : super(_openConnection());  
+
+  @override  
+  int get schemaVersion => 1;
+}
+
+  LazyDatabase _openConnection() {
+    return LazyDatabase(() async {
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, 'db.sqlite'));
+      return NativeDatabase(file);
+    });
+  }
