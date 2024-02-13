ff4371ce72a90e0f9fa9b43bf5a608f3394fa9bd
kaka0913 <kabuyuu0913@gmail.com>
2024-02-13 15:53:47 +0900

    選択されてる日にちを青色に変更

--- lib/common/calender_widget.dart
+++ lib/common/calender_widget.dart
@@ -1,10 +1,13 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:flutter_hooks/flutter_hooks.dart';
+
 // Project imports:
 import 'package:calender_application/model/calender_model.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends HookWidget {
   const Calendar({
     required this.date,
     super.key,
@@ -14,14 +17,22 @@ class Calendar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final today = DateTime.now();
+    final selectedDate = useState<DateTime?>(today);
     final calendarData = CalendarBuilder().build(date);
 
     return Column(
       children: [
-        const _WeekRow(['月', '火', '水', '木', '金', '土', '日']),
+        WeekRow(
+          const ['月', '火', '水', '木', '金', '土', '日'],
+          selectedDate: null,
+          onDateSelected: (date) => selectedDate.value = date,
+        ),
         ...calendarData.map(
-          (week) => _WeekRow(
+          (week) => WeekRow(
             week.map((date) => date?.toString() ?? '').toList(),
+            selectedDate: selectedDate.value,
+            onDateSelected: (date) => selectedDate.value = date,
           ),
         ),
       ],
@@ -29,10 +40,17 @@ class Calendar extends StatelessWidget {
   }
 }
 
-class _WeekRow extends StatelessWidget {
-  const _WeekRow(this.datesOfWeek);
+class WeekRow extends StatelessWidget {
+  const WeekRow(
+    this.datesOfWeek, {
+    required this.selectedDate,
+    required this.onDateSelected,
+    super.key,
+  });
 
   final List<String> datesOfWeek;
+  final DateTime? selectedDate;
+  final ValueChanged<DateTime?> onDateSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +58,24 @@ class _WeekRow extends StatelessWidget {
       children: List.generate(
         datesOfWeek.length,
         (index) => Expanded(
-          child: _DateBox(
+          child: DateBox(
             datesOfWeek[index],
             weekday: index + 1,
+            isSelected: selectedDate?.day == int.tryParse(datesOfWeek[index]),
+            onDateSelected: () {
+              final selectedDay = int.parse(datesOfWeek[index]);
+              if (selectedDate?.day == selectedDay) {
+                onDateSelected(null);
+              } else {
+                onDateSelected(
+                  DateTime(
+                    selectedDate?.year ?? 0,
+                    selectedDate?.month ?? 0,
+                    selectedDay,
+                  ),
+                );
+              }
+            },
           ),
         ),
       ).toList(),
@@ -50,49 +83,62 @@ class _WeekRow extends StatelessWidget {
   }
 }
 
-class _DateBox extends StatelessWidget {
-  const _DateBox(
+class DateBox extends StatelessWidget {
+  const DateBox(
     this.label, {
     required this.weekday,
+    required this.isSelected,
+    required this.onDateSelected,
+    super.key,
   });
 
   final String label;
   final int weekday;
+  final bool isSelected;
+  final VoidCallback onDateSelected;
 
   @override
   Widget build(BuildContext context) {
     final today = DateTime.now();
     final isToday = today.day == int.tryParse(label);
+    final isWeekdayLabel = ['月', '火', '水', '木', '金', '土', '日'].contains(label);
+    final isDate = int.tryParse(label) != null;
 
     return GestureDetector(
-      onTap: () {
-        // ボタンが押されたときの処理をここに書く
-      },
+      onTap: onDateSelected,
       child: AspectRatio(
         aspectRatio: 1,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              label,
-              style: TextStyle(
-                color: weekday == 6
-                    ? Colors.blue
-                    : weekday == 7
-                        ? Colors.red
-                        : Colors.black,
-              ),
-            ),
-            if (isToday)
-              Container(
-                width: 7,
-                height: 7,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  shape: BoxShape.circle,
+        child: Container(
+          decoration: BoxDecoration(
+            color: isSelected && isDate && !isWeekdayLabel
+                ? const Color.fromARGB(255, 108, 179, 237)
+                : Colors.transparent,
+            shape: BoxShape.circle,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                label,
+                style: TextStyle(
+                  color: weekday == 6
+                      ? Colors.blue
+                      : weekday == 7
+                          ? Colors.red
+                          : Colors.black,
                 ),
               ),
-          ],
+              if (isToday)
+                Container(
+                  width: 7,
+                  height: 7,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -7,6 +7,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 // Project imports:
 import 'package:calender_application/common/calender_widget.dart';
+import 'package:calender_application/view/calender_controller.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
@@ -63,7 +64,8 @@ class HomePage extends HookWidget {
                   },
                   itemBuilder: (context, index) {
                     return Calendar(
-                        date: DateTime(setDate.value.year, index + 1),);
+                      date: DateTime(setDate.value.year, index + 1),
+                    );
                   },
                 ),
               ),
@@ -71,62 +73,6 @@ class HomePage extends HookWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class CalendarController extends StatelessWidget {
-  const CalendarController({
-    required this.currentMonth,
-    required this.toCurrentMonth,
-    required this.onNextTap,
-    super.key,
-  });
-
-  final String currentMonth;
-  final VoidCallback toCurrentMonth;
-  final VoidCallback onNextTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        InkWell(
-          onTap: toCurrentMonth,
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: Colors.grey,
-              ),
-            ),
-            child: const Text(
-              '今日',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(width: 60),
-        Center(
-          child: Text(
-            currentMonth,
-            style: const TextStyle(
-              fontSize: 20,
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: onNextTap,
-          child: const Padding(
-            padding: EdgeInsets.all(8),
-            child: Icon(Icons.arrow_drop_down),
-          ),
-        ),
-      ],
     );
   }
 }
